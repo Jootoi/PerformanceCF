abebe61e4a22dd524a68d44a21b837f34e181f40
Joonas Toimela <joonas.toimela@gmail.com>
2019-11-17 19:19:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# High-Performance Library for Collaborative Filtering
---
+++ src/PerformanceCF.cpp
@@ -0,0 +1,12 @@
+﻿// PerformanceCF.cpp : Defines the entry point for the application.
+//
+
+#include "PerformanceCF.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
